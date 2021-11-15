bb1da66da862cdd5ce2923eb7dddd6dbc87b28aa
Junyeob Kim <juny116@europa.snu.ac.kr>
2021-11-15 14:09:41 +0900

    init code

---
+++ scripts/gpt2_sst2.sh
@@ -0,0 +1,24 @@
+export num_gpus=4
+export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+export PYTHONHASHSEED=0
+export output_dir="./sst2_gpt"
+export TORCH_DISTRIBUTED_DEBUG=INFO
+python -m torch.distributed.launch --nproc_per_node=$num_gpus \
+examples/text-classification/run_glue.py \
+--model_name_or_path gpt2 \
+--task_name sst2 \
+--do_train \
+--do_eval \
+--max_seq_length 128 \
+--per_device_train_batch_size 8 \
+--learning_rate 5e-4 \
+--num_train_epochs 20 \
+--output_dir $output_dir/model \
+--overwrite_output_dir \
+--logging_steps 10 \
+--logging_dir $output_dir/log \
+--evaluation_strategy epoch \
+--save_strategy epoch \
+--warmup_ratio 0.06 \
+--seed 0 \
+--weight_decay 0.1
