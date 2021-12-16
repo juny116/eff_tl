4593847af18721284236f007bc6bec79a6f5b5d8
heyjoonkim <heyjoonkim@gmail.com>
2021-12-16 06:33:03 +0000

    fix script files

--- scripts/mnli/idpg-mnli-40000.sh
+++ scripts/mnli/idpg-mnli-40000.sh
@@ -1,24 +1,19 @@
-export CUDA_VISIBLE_DEVICES=0,1,2
-
 main_model="gpt2-xl"
 
 task="mnli"
-#learning_rates="1e-4 5e-5 1e-5"
-learning_rates="1e-6"
-
+learning_rates="1e-4 1e-5"
 seed="1234"
 train_epochs="40"
-
-path="/home/heyjoonkim/data/heyjoonkim/prompt_test"
 
 # original IDPG
 for learning_rate in $learning_rates; do
     deepspeed main.py \
         --task_name $task \
         --model_name_or_path $main_model \
-        --ds_config ds_configs/zero2_config.json \
-        --output_dir $path/$task/IDPG-freezed/$learning_rate/ \
+        --ds_config ds_configs_samples/zero2_config.json \
+        --output_dir $output_dir/$task/IDPG-freezed/$learning_rate/ \
         --seed $seed \
+        --lr $learning_rate \
         --num_train_epochs $train_epochs \
         --max_train_samples 40000 \
         --apply_encoder \
